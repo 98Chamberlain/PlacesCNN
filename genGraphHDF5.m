e4a87f4be2baa2861e45cede6da394e664de9efb
98Chamberlain <a071442002@gmail.com>
2015-08-11 04:46:25 +0000

    fix function error in matlab file

--- genGraphHDF5.m
+++ genGraphHDF5.m
@@ -1,14 +1,14 @@
 load('./scene_name.mat');
 % load scene_t 205 * 2 cell
 
-tr_data = hdf5load('./graphCNNtraining.h5','data');
+tr_data = hdf5read('./graphCNNtraining.h5','data');
 % data: 205 * 17400
 % label: 40 * 17400
 for i = 1:205
     hdf5write(['./train_h5/',scene_t{i,1},'_train.h5'],'data',tr_data(i,:));
 end
 
-te_data = hdf5load('./graphCNNtesting.h5','data');
+te_data = hdf5read('./graphCNNtesting.h5','data');
 % data: 205 * 4872
 % label: 40 * 4872
 for i = 1:205
