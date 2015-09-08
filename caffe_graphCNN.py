7852e55a4adaceda52a96d3a09e43651d54eb9d3
98Chamberlain <a071442002@gmail.com>
2015-09-08 02:26:26 +0000

    To be continued. Open a new branch.

--- caffe_graphCNN.py
+++ caffe_graphCNN.py
@@ -8,7 +8,7 @@ import scipy.io as sio
 import h5py
 
 MODEL_FILE = './graphCNN_deploy.prototxt'
-PRETRAINED = './_iter_11000.caffemodel'
+PRETRAINED = './sigmoidcross_iter_10000.caffemodel'
 
 caffe.set_device(0)
 caffe.set_mode_gpu()
