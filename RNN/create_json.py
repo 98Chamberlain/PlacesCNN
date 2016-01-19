5d61757fcb86d5a95b9d5b35cafcb8505a41d792
98Chamberlain <a071442002@gmail.com>
2016-01-19 10:26:08 +0000

    edit the parameter of create_json.py

--- RNN/create_json.py
+++ RNN/create_json.py
@@ -7,6 +7,8 @@ TRAIN_IMAGE = '../genLMDB/train_with_label.txt'
 VAL_IMAGE = '../genLMDB/val_with_label.txt'
 TEST_IMAGE = '../genLMDB/test_with_label.txt'
 
+dataset = [('TRAIN','train'),('VAL','val'),('TEST','test')]
+
 def ensure_dir(d):
     if not os.path.exists(d):
         os.makedirs(d)
@@ -17,28 +19,34 @@ def file_len(fname):
         pass
     return i + 1
 	
-file = open( TRAIN_IMAGE, 'r')
+file = open( TEST_IMAGE, 'r')
 
 train_len = file_len(file)
 file.close()
-train_path = np.array([],dtype=object)
-train_label = np.array([])
-file = open( TRAIN_IMAGE, 'r')
-for i, line in enumerate(file):
+print 'got the length ', train_len
+
+train_path = np.zeros(train_len,dtype=object)
+train_label = np.zeros(train_len)
+file = open( TEST_IMAGE, 'r')
+for i,line in enumerate(file):
 
     # extract the file name
     txt_split = line.split()
-    train_path = np.append( train_path , txt_split[0] )
-	train_label = np.append( train_label , txt_split[1] ) # label in 205
+    train_path[i] = txt_split[0]
+    train_label[i] = txt_split[1] # label in 205
 	
 print 'extracted the image path and label'
 	
 index = [i for i in range(train_len)]
 random.shuffle(index)
-index = numpy.array(index)
+index = np.array(index)
 
-train_path_shuffle = train_path(index)
-train_label_shuffle = train_label(index)
+train_path_shuffle = train_path[index]
+train_label_shuffle = train_label[index]
+
+id_file = open( './test_id.txt' , 'w')
+for i in index:
+    id_file.write("%d\n" %i)
 
 print 'shuffled the data'
 
@@ -55,18 +63,18 @@ licenses = {"id": 1, "name": 'Places Dataset', "url": 'http://places.csail.mit.e
 print 'set information in json type'
 
 # produce the image list in json type
-images = []
+images = [{} for i in range(train_len)]
 for i in range(train_len):
 	
 	image = {"id": i, "width": 256, "height": 256, "file_name": train_path_shuffle[i], \
 	         "license": 1, "flickr_url": 'none', "coco_url": 'none', "date_captured": '2015-12-29 05:00:00.000000',}
-	images.append(image)
+	images[i] = image
 	
 print 'converted the image list to json type'
 print images[0]
 
 # produce the annotations list in json type
-annotations = []
+annotations = [{} for i in range(train_len)]
 label_file = open( './scene_label.txt', 'r')
 for line in label_file:
     label_list = line.split()
@@ -82,8 +90,8 @@ gt_scene = mat['gt_scene'][0] # usage: gt_scene[i] for i-th scene
 groundtruth = mat['groundtruth'][0] # usage: groundtruth[i][0] for i-th label
 for i in range(train_len):
 	
-	scn_index = train_label_shuffle[i]
-	if scn_index == 94:
+    scn_index = train_label_shuffle[i]
+    if scn_index == 94:
         gt = np.array([1,2,6,7])
     elif scn_index == 123:
         gt = np.array([1,3,4,38,39])
@@ -93,15 +101,16 @@ for i in range(train_len):
         gt = np.array([1,2,5])
     elif (sum([48,66,91,103,162,164] == scn_index) >= 1):
         gt = np.array([1,3,4,12,31,35])
-    else
-        gt = groundtruth[gt_scene[scn_index]][0]
+    else:
+        gt = groundtruth[gt_scene[scn_index-1]-1][0]
 
-	annotation = {"id": i, "image_id": i, "caption":' '.join(gt)}
-	annotations.append(annotation)
+    caption = ' '.join(label_list[gt-1])
+    annotation = {"id": i, "image_id": i, "caption":caption}
+    annotations[i] = annotation
 
 print 'converted the annotation list to json type'
 print annotations[0]
 	
 data = {"info": info, "licenses": licenses, "images": images, "annotations": annotations}
-with open('./train_scn.json','w') as outfile:
-	json.dump(data, outfile)
+with open('./test_scn.json','w') as outfile:
+    json.dump(data, outfile)
